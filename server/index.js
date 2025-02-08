9a39398a6d6acd462124ae346589e4b34b4bdd29
Silverbelly <jonnoel@outlook.com>
2025-02-07 18:08:38 -0600

    initial commit - server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
